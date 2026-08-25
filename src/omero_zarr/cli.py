04d26917712941046ecec042ba26cf74971aeb8a
Dominik Lindner <d.lindner@dundee.ac.uk>
2026-08-25 11:35:05 +0100

    fix help text for parseuri

--- src/omero_zarr/cli.py
+++ src/omero_zarr/cli.py
@@ -357,9 +357,12 @@ class ZarrControl(BaseControl):
         import_cmd.add_argument(
             "--parseuri",
             action="store_true",
-            help="Try to parse the URI to extract endpoint, bucket and path "
-            "in order to  create a S3 URI (e.g. from an HTTP URI) "
-            "(default: Just use the URI as-is)",
+            help=(
+                "Try to parse the URI to extract endpoint, bucket and path "
+                "in order to create a S3 URI (e.g. from an HTTP URI), needed for "
+                "omero-zarr-pixel-buffer versions < 0.7 which didn't support http "
+                "(default: Just use the URI as-is)"
+            ),
         )
         import_cmd.add_argument(
             "--target",
